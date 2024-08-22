8885dff176b1ae3675a0eb8c7d368ee565a4be80
Jonas <jonashalvorsen2013@live.no>
2024-08-22 11:59:32 +0200

    Exercise-complete

--- tdd-oop-polymorphism.CSharp.Main/Basket.cs
+++ tdd-oop-polymorphism.CSharp.Main/Basket.cs
@@ -8,74 +8,21 @@ namespace tdd_oop_polymorphism.CSharp.Main
 {
     public class Basket
     {
-        List<Game> games = new List<Game>();
-        List<Drink> drinks = new List<Drink>();
-        List<Book> books = new List<Book>();
+        private List<IProduct> _products = new List<IProduct>();
 
-        public void add(Game game)
+        public void add(IProduct product)
         {
-            this.games.Add(game);
+            _products.Add(product);
         }
 
-        public void add(Drink drink)
+        public int getTotal() 
         {
-            this.drinks.Add(drink);
-        }
-
-        public void add(Book book)
-        {
-            this.books.Add(book);
-        }
-
-        public int getTotal()
-        {
-            int total = 0;
-
-            foreach (Game game in this.games)
-            {
-                total += game.getPrice();
-            }
-
-            foreach (Drink drink in this.drinks)
-            {
-                total += drink.getPrice();
-            }
-
-            foreach (Book book in this.books)
-            {
-                total += book.getPrice();
-            }
-
-            return total;
+            return _products.Sum(x => x.Price);
         }
 
         public bool isInBasket(String name)
         {
-            foreach (Game game in this.games)
-            {
-                if (game.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
-
-            foreach (Drink drink in this.drinks)
-            {
-                if (drink.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
-
-            foreach (Book book in this.books)
-            {
-                if (book.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return _products.Any(x => x.Name == name);
         }
     }
 }
